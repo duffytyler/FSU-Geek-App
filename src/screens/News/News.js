5d99594c564e199fb3f49f862b582f7bdcd8c1c9
duffytyler <td20e@fsu.edu>
2022-10-16 15:41:20 -0400

    Older news without fetch

--- src/screens/News/News.js
+++ src/screens/News/News.js
@@ -1,51 +1,42 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, StyleSheet, ImageBackground, ScrollView, SafeAreaView, Text} from 'react-native';
+import { View, StyleSheet, ImageBackground, ScrollView, StatusBar, SafeAreaView, Dimensions} from 'react-native';
+import CustomButton from '../../components/CustomButton/CustomButton'
 import Background from '../../../assets/images/newsbg.jpg';
+import { Auth } from 'aws-amplify';
 import Blocks from '../../components/Blocks/Blocks'
+import TitleImage from '../../../assets/images/testnews.jpeg'
 const News = () => {
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true)
-    const url = 'http://10.0.0.231:8000/Articles'
-    useEffect(()=>{
-        fetch(url)
-        .then((response) => response.json())
-        .then((json) =>setData(json))
-        .catch((error) => console.error(error))
-        .finally(()=> setLoading(false));
-    },[])
+    
     const navigation = useNavigation();
     return(
     //go to Blocks in components to edit Blocks formatting
     //scrollview height still needs to be fixed
-   <ImageBackground source= {Background} style = {styles.background}>
+   <ImageBackground source= {Background} resizeMode = "cover" style = {{width:"100%", height:"100%"}}>
     <View style={{flex:1}}>
-            <ScrollView showsVerticalScrollIndicator={false} 
-                bounces={false}
-                automaticallyAdjustContentInsets={false}
-                contentContainerStyle={{flexGrow:1}}>
-                   <SafeAreaView style={styles.container}>
-                    {loading ? ( <Text>Loading...</Text>): (
-                        data.map((Articles)=>(
-                            <Blocks title = {Articles.titles} url = {Articles.url} image = {{uri:Articles.img}} type = "article"/> 
-                    ))
-                )}
-                </SafeAreaView>
-                </ScrollView>
-                </View>
+       <ScrollView showsVerticalScrollIndicator={false} 
+       automaticallyAdjustContentInsets={false}
+       bounces={false}
+       contentContainerStyle = {{flexGrow:1}}>
+            <SafeAreaView style={styles.container}>
+                <Blocks title = "Article Title" url = "https://news.fsu.edu/" image = {TitleImage} type = "article" /> 
+                <Blocks title = "Article Title" url = "test" image = {TitleImage} type = "article" /> 
+                <Blocks title = "Article Title" url = "test" image = {TitleImage} type = "article" /> 
+                <Blocks title = "Article Title" url = "test" image = {TitleImage} type = "article" /> 
+                <Blocks title = "Article Title" url = "test" image = {TitleImage} type = "article" /> 
+            </SafeAreaView>
+        </ScrollView>
+    </View>
     </ImageBackground>
     )
 
 }
 
 const styles = StyleSheet.create({
-    background:
-    {
-        height:'100%', 
-        width:'100%', 
-    },
     container:{
+        justifyContent:"flex-start",
         alignItems:"center",
+        paddingTop: StatusBar.currentHeight,
     },
     
 })

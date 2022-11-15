fd92b490b2b74e1cf17b44cab6d3223ebcdf709f
duffytyler <td20e@fsu.edu>
2022-11-15 13:06:09 -0500

    Update Advising.js

--- src/screens/Advising/Advising.js
+++ src/screens/Advising/Advising.js
@@ -16,7 +16,18 @@ const navigation = useNavigation();
        bounces={false}
        contentContainerStyle = {{flexGrow:1}}>
             <SafeAreaView style={styles.container}>
-                <Blocks title = "Advising Help" type = "large" url = "https://advisingfirst.fsu.edu/" image={AdvisingImage} numberoflines={6} 
+                <Blocks title = "FSU:Tallahassee - Advising Help" type = "large" url = "https://advisingfirst.fsu.edu/" image={AdvisingImage} 
+                 description="Academic advising help is available to all FSU students. The Advising Office
+                 provides students with a vast array of helpful programs, resources, and services.
+                 In addition, the Advising team will help you make the most important decisions
+                 during your academic career, such as choosing the right courses to help keep
+                 you on track for graduation along with much more.
+                 \n
+                 \n
+                 \nNote : Advising for the Tallahassee Campus and the Panama City Campus are different.
+                 in order to see a list of Panama City advisors or book an appointment, please go to : " />
+
+                 <Blocks title = "FSU:PC - Advising Help" type = "large" url = "https://pc.fsu.edu/bookings" image={AdvisingImage} 
                  description="Academic advising help is available to all FSU students. The Advising Office
                  provides students with a vast array of helpful programs, resources, and services.
                  In addition, the Advising team will help you make the most important decisions
